02f572867a173f9deb1b84cf4d155b580c4efeaf
yateesh-chandra-duggirala <yateeshchandraduggirala3@gmail.com>
2024-02-24 00:07:18 +0530

    Django Static Files with WhiteNoise

---
+++ first_project/members/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.utils import timezone
+
+class Members(models.Model):
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    phone = models.IntegerField(null = True)
+    entry_Date = models.DateField(default = timezone.now, null = True)
+
+def __str__(self):
+    return f"{self.firstname} {self.lastname}"
