39e69073d7163a2b3ab439c83a0a0d6371bbafe4
yateesh-chandra-duggirala <yateeshchandraduggirala3@gmail.com>
2024-02-23 23:56:48 +0530

    Files listed

--- first_project/members/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Members
-
-# Register your models here.
-class MemberAdmin(admin.ModelAdmin):
-    list_display = ("firstname","lastname", "entry_Date")
-
-admin.site.register(Members, MemberAdmin)
